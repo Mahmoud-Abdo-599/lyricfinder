dfa81a70f0d50d2866cacefb49f87dc330012fb1
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-04-11 00:14:51 +0200

    Updates

---
+++ precache-manifest.f2b609bf8822ff807de60e36d1d83fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c79d630a48158909cfc876a33a4bd2",
+    "url": "/lyricfinder/index.html"
+  },
+  {
+    "revision": "90edcbe3789b98308e20",
+    "url": "/lyricfinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2457fbb1d585d44aff0c",
+    "url": "/lyricfinder/static/js/2.d71b5874.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/lyricfinder/static/js/2.d71b5874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90edcbe3789b98308e20",
+    "url": "/lyricfinder/static/js/main.fb6d3aac.chunk.js"
+  },
+  {
+    "revision": "9b6b1771a671d97dd64e",
+    "url": "/lyricfinder/static/js/runtime-main.3ac43bd9.js"
+  }
+]);
